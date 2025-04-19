914c644a1434bf4dba1d00defc51c8d045c71791
Pawel Potepa <potepapawel@gmail.com>
2025-04-19 10:00:00 +0200

    Add Pipe Handler

---
+++ src/App/Engine/Runtime/Pipe/PipeThreadHandler.cs
@@ -0,0 +1,88 @@
+using MessagePack.Resolvers;
+using MessagePack;
+using ORBIT9000.Engine.Runtime.State;
+using System.IO.Pipes;
+using ORBIT9000.Core.Models.Pipe;
+
+namespace ORBIT9000.Engine.Strategies.Running
+{
+    internal class PipeThreadHandler
+    {
+        private readonly EngineState _state;
+
+        public PipeThreadHandler(EngineState state)
+        {
+            _state = state ?? throw new ArgumentNullException(nameof(state));
+
+            if (_state.Engine is null)
+            {
+                throw new InvalidOperationException("Engine state is null.");
+            }
+        }
+
+        public async Task StartAsync()
+        {
+            _state.Engine.LogInformation("Starting PipeThread.");
+
+            try
+            {
+                using var server = new NamedPipeServerStream("OrbitEngine", PipeDirection.Out);
+
+                await server.WaitForConnectionAsync();
+                _state.Engine.LogInformation("GUI Connected");
+
+                var random = new Random();
+
+                while (_state.Engine.IsRunning)
+                {
+                    try
+                    {
+                        var options = MessagePackSerializerOptions.Standard.WithResolver(CompositeResolver.Create(
+                            ContractlessStandardResolver.Instance,
+                            StandardResolver.Instance
+                        ));
+
+                        var exampleData = new ExampleData
+                        {
+                            Frame1 = new SettingsData
+                            {
+                                Setting1 = random.Next(1, 100),
+                                Setting2 = random.Next(1, 100).ToString(),
+                            },
+
+                            Frame2 = new EngineData
+                            {
+                                Setting1 = random.Next(1, 100),
+                                Setting2 = random.Next(1, 100)
+                            }
+                        };
+
+                        byte[] buffer = MessagePack.MessagePackSerializer.Serialize(exampleData, options);
+
+                        if (random.NextDouble() > 0.97)
+                        {
+                            await server.WriteAsync(buffer, 0, buffer.Length);
+                            _state.Engine.LogDebug("Message sent to GUI.");
+                        }
+                        else
+                        {
+                            _state.Engine.LogDebug("Message skipped for this interval.");
+                        }
+
+                        await Task.Delay(TimeSpan.FromMilliseconds(random.Next(50, 200)));
+                    }
+                    catch (Exception ex)
+                    {
+                        _state.Engine.LogError("An error occurred during the pipe thread loop: {Message}", ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _state.Engine.LogError("An error occurred in PipeThread: {Message}", ex.Message);
+            }
+
+            _state.Engine.LogInformation("PipeThread has completed.");
+        }
+    }
+}
--- src/App/Engine/Runtime/Strategies/Running/DefaultEngineStrategy.cs
+++ src/App/Engine/Runtime/Strategies/Running/DefaultEngineStrategy.cs
@@ -19,7 +19,7 @@ namespace ORBIT9000.Engine.Strategies.Running
 
             if (state.Engine.Configuration.EnableTerminal)
             {
-                Task.Run(() => PipeThread(state));
+                Task.Run(() => new PipeThreadHandler(state).StartAsync());
             }
 
             state.Engine.LogInformation("Engine is running. Strategy {Strategy}", nameof(EngineStartupStrategy));
@@ -80,63 +80,6 @@ namespace ORBIT9000.Engine.Strategies.Running
             engine.LogInformation("Initializing engine.");
             // Any initialization logic here
             engine.LogInformation("Engine initialization completed.");
-        }
-
-        private static async Task PipeThread(EngineState? state)
-        {
-            if (state is null || state.Engine is null)
-            {
-                throw new InvalidOperationException("Engine state is null.");
-            }
-
-            state.Engine.LogInformation("Starting PipeThread.");
-
-            using var server = new NamedPipeServerStream("OrbitEngine", PipeDirection.Out);
-
-            await server.WaitForConnectionAsync();
-            state.Engine.LogInformation($"GUI Connected");
-
-            var random = new Random();
-
-            while (state.Engine.IsRunning)
-            {
-                var options = MessagePackSerializerOptions.Standard.WithResolver(CompositeResolver.Create(
-                    ContractlessStandardResolver.Instance,
-                    StandardResolver.Instance
-                ));
-
-                var exampleData = new ExampleData
-                {
-                    Frame1 = new SettingsData
-                    {
-                        Setting1 = random.Next(1, 100),
-                        Setting2 = "Text2",
-                    },
-
-                    Frame2 = new EngineData
-                    {
-                        Setting1 = random.Next(1, 100),
-                        Setting2 = random.Next(1, 100)
-                    }
-                };
-
-                byte[] buffer = MessagePack.MessagePackSerializer.Serialize(exampleData, options);
-
-                
-                if (random.NextDouble() > 0.97) 
-                {
-                    await server.WriteAsync(buffer, 0, buffer.Length);
-                    state.Engine.LogDebug("Message sent to GUI.");
-                }
-                else
-                {
-                    state.Engine.LogDebug("Message skipped for this interval.");
-                }
-
-                await Task.Delay(TimeSpan.FromMilliseconds(random.Next(50, 200))); 
-            }
-
-            state.Engine.LogInformation("PipeThread has completed.");
         }
     }
 }
