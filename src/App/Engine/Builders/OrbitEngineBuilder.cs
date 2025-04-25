d5fa350e8572e0430ee5d3007ab3c4036e67e84e
Pawel Potepa <potepapawel@gmail.com>
2025-04-25 11:31:29 +0200

    Remove unused parts of code

--- src/App/Engine/Builders/OrbitEngineBuilder.cs
+++ src/App/Engine/Builders/OrbitEngineBuilder.cs
@@ -49,8 +49,11 @@ namespace ORBIT9000.Engine.Builders
             _services.AddSingleton<IPluginProvider, PluginProvider>();
             _services.AddLogging();
 
-            _services.AddSingleton<IPluginLoader>(provider 
-                => provider.GetRequiredService<PluginLoaderFactory>().Create());
+            _services.AddSingleton<IPluginLoader>(provider
+                =>
+            {
+                return provider.GetRequiredService<PluginLoaderFactory>().Create();
+            });
 
 
             _services.AddSingleton<OrbitEngine>();
