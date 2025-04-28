f9f63eadc383e689615a0a129f4e53d88bad2ee1
Pawel Potepa <potepapawel@gmail.com>
2025-04-28 10:39:24 +0200

    Add Simple Html View Parsing to Terminal PoC

--- src/App/GUI/EngineTerminal/Program.cs
+++ src/App/GUI/EngineTerminal/Program.cs
@@ -1,114 +1,84 @@
-﻿using EngineTerminal.Views;
-using MessagePack;
-using ORBIT9000.Engine.Configuration;
-using System.Buffers;
-using System.IO.Pipes;
-using Terminal.Gui;
+﻿using Terminal.Gui;
 
 namespace Orbit9000.EngineTerminal
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Application.Init();
+         
+            var colorScheme = new ColorScheme
+            {
+                Normal = Application.Driver.MakeAttribute(Color.White, Color.Blue),
+                Focus = Application.Driver.MakeAttribute(Color.Gray, Color.DarkGray),
+                HotNormal = Application.Driver.MakeAttribute(Color.BrightBlue, Color.Blue),
+                HotFocus = Application.Driver.MakeAttribute(Color.BrightYellow, Color.DarkGray)
+            };
+
+            Application.Current.ColorScheme = colorScheme;
 
             var top = Application.Top;
 
-            var menu = new MenuBar(new MenuBarItem[]
-            {
-                    new MenuBarItem("Engine", new MenuItem[]
-                    {
-                        new MenuItem("Start", "", () => {}),
-                        new MenuItem("Stop", "", () => {})
-                    }),
+            var html = System.IO.File.ReadAllText("./views/index.html");
 
-                    new MenuBarItem("Plugins", new MenuItem[]
-                    {
-                        new MenuItem("Load", "", () => {}),
-                        new MenuItem("Unload", "", () => {}),
-                        new MenuItem("Activity", "", () => {})
-                    }),
-                    new MenuBarItem("Diagnostics", new MenuItem[]
-                    {
-                        new MenuItem("UpTime", "", () => {})
-                    })
-            });
+            var translator = new Translator();
+            var mainView = translator.Translate(html);
 
-            top.Add(menu);
-
-            var textView = new TextView
-            {
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
-                ReadOnly = true
-            };
-
-            var mainWindow = new MainWindow();
-            top.Add(mainWindow);
-
-            var window = new Window("Engine Terminal")
-            {
-                Width = Dim.Fill(),
-                Height = Dim.Fill()
-            };
-
-            window.Add(textView);
-            top.Add(window);
-
-            var backgroundThread = new Thread(() =>
-            {
-                var client = new NamedPipeClientStream(".", "OrbitEngine", PipeDirection.In);
-
-                try
-                {
-                    client.Connect();
-                    Application.MainLoop.Invoke(() => textView.Text += "Connected to engine!\n");
-
-                    var buffer = new byte[4096];
-
-                    while (true)
-                    {
-                        try
-                        {
-                            int bytesRead = client.Read(buffer, 0, buffer.Length);
-                            if (bytesRead == 0)
-                            {
-                                Application.MainLoop.Invoke(() => textView.Text += "Server closed connection.\n");
-                                break;
-                            }
-
-                            List<PluginInfo> @object = MessagePackSerializer.Deserialize<List<PluginInfo>>(new ReadOnlySequence<byte>(buffer, 0, bytesRead));
-
-                            Application.MainLoop.Invoke(() =>
-                            {
-                                mainWindow.AddText($"Received Engine state: {@object.Count}\n");
-
-                                foreach (var pluginInfo in @object)
-                                {
-                                    textView.Text += $"Plugin: {pluginInfo.PluginType}, Activated: {pluginInfo.Activated}\n";
-                                }
-                            });
-                        }
-                        catch (IOException ex)
-                        {
-                            Application.MainLoop.Invoke(() => textView.Text += $"Pipe broken: {ex.Message}\n");
-                            break;
-                        }
-
-                        Thread.Sleep(1000);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Application.MainLoop.Invoke(() => textView.Text += $"Error connecting to pipe: {ex.Message}\n");
-                }
-            });
-
-            backgroundThread.IsBackground = true;
-            backgroundThread.Start();
+            top.Add(mainView);
 
             Application.Run();
         }
+
+        #region PipeRegion
+        //var backgroundThread = new Thread(() =>
+        //    {
+        //        var client = new NamedPipeClientStream(".", "OrbitEngine", PipeDirection.In);
+
+        //        try
+        //        {
+        //            client.Connect();
+        //            Application.MainLoop.Invoke(() => textView.Text += "Connected to engine!\n");
+
+        //            var buffer = new byte[4096];
+
+        //            while (true)
+        //            {
+        //                try
+        //                {
+        //                    int bytesRead = client.Read(buffer, 0, buffer.Length);
+        //                    if (bytesRead == 0)
+        //                    {
+        //                        Application.MainLoop.Invoke(() => textView.Text += "Server closed connection.\n");
+        //                        break;
+        //                    }
+
+        //                    List<PluginInfo> @object = MessagePackSerializer.Deserialize<List<PluginInfo>>(new ReadOnlySequence<byte>(buffer, 0, bytesRead));
+
+        //                    Application.MainLoop.Invoke(() =>
+        //                    {
+        //                        textView.Text += ($"Received Engine state: {@object.Count}\n");
+
+        //                        foreach (var pluginInfo in @object)
+        //                        {
+        //                            textView.Text += $"Plugin: {pluginInfo.PluginType}, Activated: {pluginInfo.Activated}\n";
+        //                        }
+        //                    });
+        //                }
+        //                catch (IOException ex)
+        //                {
+        //                    Application.MainLoop.Invoke(() => textView.Text += $"Pipe broken: {ex.Message}\n");
+        //                    break;
+        //                }
+
+        //                Thread.Sleep(1000);
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Application.MainLoop.Invoke(() => textView.Text += $"Error connecting to pipe: {ex.Message}\n");
+        //        }
+        //    }); 
+        #endregion
     }
 }
---
+++ src/App/GUI/EngineTerminal/Translator.cs
@@ -0,0 +1,157 @@
+﻿using HtmlAgilityPack;
+using Terminal.Gui;
+
+public class Translator
+{
+    private readonly Dictionary<string, Func<HtmlNode, View>> _tagHandlers;
+
+    public Translator()
+    {
+        _tagHandlers = new Dictionary<string, Func<HtmlNode, View>>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "nav", CreateNavbar },
+            { "section", CreateSection },
+            { "h1", node => CreateLabel("# " + node.InnerText.Trim()) },
+            { "h2", node => CreateLabel("## " + node.InnerText.Trim()) },
+            { "h3", node => CreateLabel("### " + node.InnerText.Trim()) },
+            { "p", node => CreateLabel(node.InnerText.Trim()) },
+            { "ul", CreateListView },
+            { "li", node => CreateLabel("- " + node.InnerText.Trim()) }
+        };
+    }
+
+    public View Translate(string html)
+    {
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+
+        var root = new View()
+        {
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+
+        MenuBar navbar = null;
+
+        foreach (var node in doc.DocumentNode.SelectNodes("//body/*") ?? throw new ArgumentException("Tag body not found"))
+        {
+            var childView = TranslateNode(node);
+            if (childView is MenuBar menuBar)
+            {
+                navbar = menuBar;
+            }
+            else if (childView != null)
+            {
+                root.Add(childView);
+            }
+        }
+
+        if (navbar != null)
+        {
+            root.Add(navbar);
+        }
+
+        return root;
+    }
+
+    private Label CreateLabel(string text)
+    {
+        return new Label(text)
+        {
+            Width = Dim.Fill(),
+            Height = 1
+        };
+    }
+
+    private ListView CreateListView(HtmlNode node)
+    {
+        var items = new List<string>();
+
+        foreach (var li in node.SelectNodes(".//li"))
+        {
+            items.Add(li.InnerText.Trim());
+        }
+
+        return new ListView(items)
+        {
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+    }
+
+    private MenuBar CreateNavbar(HtmlNode node)
+    {
+        var menuItems = new List<MenuBarItem>();
+
+        foreach (var li in node.SelectNodes(".//li"))
+        {
+            var link = li.SelectSingleNode(".//a");
+            if (link != null)
+            {
+                menuItems.Add(new MenuBarItem(link.InnerText.Trim(), new MenuItem[] { }));
+            }
+        }
+
+        return new MenuBar(menuItems.ToArray());
+    }
+
+    private View CreateSection(HtmlNode node)
+    {
+        var section = new View()
+        {
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+
+        var header = node.SelectSingleNode(".//h1 | .//h2 | .//h3");
+        if (header != null)
+        {
+            section.Add(CreateLabel(header.InnerText.Trim()));
+        }
+
+        var paragraph = node.SelectSingleNode(".//p");
+        if (paragraph != null)
+        {
+            section.Add(CreateLabel(paragraph.InnerText.Trim()));
+        }
+
+        return section;
+    }
+
+    private View TranslateNode(HtmlNode node)
+    {
+        if (_tagHandlers.TryGetValue(node.Name, out var handler))
+        {
+            var currentView = handler(node);
+
+            foreach (var childNode in node.ChildNodes)
+            {
+                var childView = TranslateNode(childNode);
+                if (childView != null)
+                {
+                    currentView.Add(childView);
+                }
+            }
+
+            return currentView;
+        }
+
+        // Default behavior for unsupported tags
+        var defaultView = new View()
+        {
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+
+        foreach (var childNode in node.ChildNodes)
+        {
+            var childView = TranslateNode(childNode);
+            if (childView != null)
+            {
+                defaultView.Add(childView);
+            }
+        }
+
+        return defaultView;
+    }
+}
