3dbc41d5ea8cc2a14b07c5e1c4d61a79b0c56980
Pawel Potepa <potepapawel@gmail.com>
2025-05-11 13:00:00 +0200

    Add Basic Contracts

--- src/Data/IDataRetreiver.cs
+++ src/Data/IDataRetreiver.cs
@@ -1,4 +1,17 @@
 ﻿namespace ORBIT9000.Data
 {
-    internal interface IDataRetreiver;
+    internal interface IDataRetreiver
+    {
+        object FetchData(string query);
+    }
+
+    internal interface IDataProcessor
+    {
+        object ProcessData(object data);
+    }
+
+    internal interface IDataUpdater
+    {
+        bool UpdateData(object data);
+    }
 }
