dad9efcb94ae0c089b215f596541339284a87653
Pawel Potepa <potepapawel@gmail.com>
2025-05-11 15:34:14 +0200

    Update Exception Messages

--- src/App/Engine/IO/Loaders/Plugin/VoidAssembly.cs
+++ src/App/Engine/IO/Loaders/Plugin/VoidAssembly.cs
@@ -6,158 +6,158 @@ namespace ORBIT9000.Engine.IO.Loaders.Plugin
 {
     internal class VoidAssembly : Type
     {
-        public override Assembly Assembly => throw new NotImplementedException("VoidAssembly does not implement Assembly.");
+        public override Assembly Assembly => throw new NotImplementedException("Nobody expected you here.");
 
-        public override string? AssemblyQualifiedName => throw new NotImplementedException("VoidAssembly does not implement AssemblyQualifiedName.");
+        public override string? AssemblyQualifiedName => throw new NotImplementedException("Nobody expected you here.");
 
-        public override Type? BaseType => throw new NotImplementedException("VoidAssembly does not implement BaseType.");
+        public override Type? BaseType => throw new NotImplementedException("Nobody expected you here.");
 
-        public override string? FullName => throw new NotImplementedException("VoidAssembly does not implement FullName.");
+        public override string? FullName => throw new NotImplementedException("Nobody expected you here.");
 
-        public override Guid GUID => throw new NotImplementedException("VoidAssembly does not implement GUID.");
+        public override Guid GUID => throw new NotImplementedException("Nobody expected you here.");
 
-        public override Module Module => throw new NotImplementedException("VoidAssembly does not implement Module.");
+        public override Module Module => throw new NotImplementedException("Nobody expected you here.");
 
-        public override string? Namespace => throw new NotImplementedException("VoidAssembly does not implement Namespace.");
+        public override string? Namespace => throw new NotImplementedException("Nobody expected you here.");
 
-        public override Type UnderlyingSystemType => throw new NotImplementedException("VoidAssembly does not implement UnderlyingSystemType.");
+        public override Type UnderlyingSystemType => throw new NotImplementedException("Nobody expected you here.");
 
-        public override string Name => throw new NotImplementedException("VoidAssembly does not implement Name.");
+        public override string Name => throw new NotImplementedException("Nobody expected you here.");
 
         public override ConstructorInfo[] GetConstructors(BindingFlags bindingAttr)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetConstructors.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override object[] GetCustomAttributes(bool inherit)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetCustomAttributes.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override object[] GetCustomAttributes(Type attributeType, bool inherit)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetCustomAttributes with attributeType.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override Type? GetElementType()
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetElementType.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override EventInfo? GetEvent(string name, BindingFlags bindingAttr)
         {
-            throw new NotImplementedException($"VoidAssembly does not implement GetEvent for event name '{name}'.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override EventInfo[] GetEvents(BindingFlags bindingAttr)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetEvents.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override FieldInfo? GetField(string name, BindingFlags bindingAttr)
         {
-            throw new NotImplementedException($"VoidAssembly does not implement GetField for field name '{name}'.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override FieldInfo[] GetFields(BindingFlags bindingAttr)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetFields.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         [return: DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.Interfaces)]
         public override Type? GetInterface(string name, bool ignoreCase)
         {
-            throw new NotImplementedException($"VoidAssembly does not implement GetInterface for interface name '{name}'.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override Type[] GetInterfaces()
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetInterfaces.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override MemberInfo[] GetMembers(BindingFlags bindingAttr)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetMembers.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override MethodInfo[] GetMethods(BindingFlags bindingAttr)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetMethods.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override Type? GetNestedType(string name, BindingFlags bindingAttr)
         {
-            throw new NotImplementedException($"VoidAssembly does not implement GetNestedType for nested type name '{name}'.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override Type[] GetNestedTypes(BindingFlags bindingAttr)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetNestedTypes.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override PropertyInfo[] GetProperties(BindingFlags bindingAttr)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetProperties.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override object? InvokeMember(string name, BindingFlags invokeAttr, Binder? binder, object? target, object?[]? args, ParameterModifier[]? modifiers, CultureInfo? culture, string[]? namedParameters)
         {
-            throw new NotImplementedException($"VoidAssembly does not implement InvokeMember for member name '{name}'.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         public override bool IsDefined(Type attributeType, bool inherit)
         {
-            throw new NotImplementedException($"VoidAssembly does not implement IsDefined for attribute type '{attributeType}'.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override TypeAttributes GetAttributeFlagsImpl()
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetAttributeFlagsImpl.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override ConstructorInfo? GetConstructorImpl(BindingFlags bindingAttr, Binder? binder, CallingConventions callConvention, Type[] types, ParameterModifier[]? modifiers)
         {
-            throw new NotImplementedException("VoidAssembly does not implement GetConstructorImpl.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override MethodInfo? GetMethodImpl(string name, BindingFlags bindingAttr, Binder? binder, CallingConventions callConvention, Type[]? types, ParameterModifier[]? modifiers)
         {
-            throw new NotImplementedException($"VoidAssembly does not implement GetMethodImpl for method name '{name}'.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override PropertyInfo? GetPropertyImpl(string name, BindingFlags bindingAttr, Binder? binder, Type? returnType, Type[]? types, ParameterModifier[]? modifiers)
         {
-            throw new NotImplementedException($"VoidAssembly does not implement GetPropertyImpl for property name '{name}'.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override bool HasElementTypeImpl()
         {
-            throw new NotImplementedException("VoidAssembly does not implement HasElementTypeImpl.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override bool IsArrayImpl()
         {
-            throw new NotImplementedException("VoidAssembly does not implement IsArrayImpl.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override bool IsByRefImpl()
         {
-            throw new NotImplementedException("VoidAssembly does not implement IsByRefImpl.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override bool IsCOMObjectImpl()
         {
-            throw new NotImplementedException("VoidAssembly does not implement IsCOMObjectImpl.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override bool IsPointerImpl()
         {
-            throw new NotImplementedException("VoidAssembly does not implement IsPointerImpl.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
 
         protected override bool IsPrimitiveImpl()
         {
-            throw new NotImplementedException("VoidAssembly does not implement IsPrimitiveImpl.");
+            throw new NotImplementedException("Nobody expected you here.");
         }
     }
 }
