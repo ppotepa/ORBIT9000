a861a34da44037dd6a005c4f1cb2c7a8fac692a2
Paweł Potępa <potepapawel@gmail.com>
2025-05-13 13:00:09 +0200

    feat(ExampleData): add constructor and update properties
    The `ExampleData` class now includes a constructor that initializes `Frame1` and `Frame2`, ensuring they are not null. The default constructor has been removed, and the interface `IPipeData` has been added to define the same properties.
    
    Tested by creating instances of `ExampleData` and verifying property initialization.
    
    ExampleData.cs

--- src/App/Core/Models/Pipe/ExampleData.cs
+++ src/App/Core/Models/Pipe/ExampleData.cs
@@ -1,30 +1,27 @@
 ﻿namespace ORBIT9000.Core.Models.Pipe
 {
-    namespace ORBIT9000.Core.Models.Pipe
+    public class ExampleData : IPipeData
     {
-        public class ExampleData : IPipeData
+        public ExampleData()
         {
-            public ExampleData()
-            {
-            }
-
-            public ExampleData(SettingsData frame1, EngineData frame2)
-            {
-                this.Frame1 = frame1 ?? throw new ArgumentNullException(nameof(frame1));
-                this.Frame2 = frame2 ?? throw new ArgumentNullException(nameof(frame2));
-            }
-
-            [MessagePack.Key(0)]
-            public SettingsData? Frame1 { get; set; }
-
-            [MessagePack.Key(1)]
-            public EngineData? Frame2 { get; set; }
         }
 
-        public interface IPipeData
+        public ExampleData(SettingsData frame1, EngineData frame2)
         {
-            SettingsData? Frame1 { get; set; }
-            EngineData? Frame2 { get; set; }
+            this.Frame1 = frame1 ?? throw new ArgumentNullException(nameof(frame1));
+            this.Frame2 = frame2 ?? throw new ArgumentNullException(nameof(frame2));
         }
+
+        [MessagePack.Key(0)]
+        public SettingsData? Frame1 { get; set; }
+
+        [MessagePack.Key(1)]
+        public EngineData? Frame2 { get; set; }
+    }
+
+    public interface IPipeData
+    {
+        SettingsData? Frame1 { get; set; }
+        EngineData? Frame2 { get; set; }
     }
 }
