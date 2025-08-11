9eb35397983c9bdfc16656ca4e0a62319f97e90e
PPOTEPA-DEV\pawel <potepapawel@gmail.com>
2025-08-11 23:11:12 +0200

    no message

--- src/App/Engine/Loaders/Assembly/AssemblyLoader.cs
+++ src/App/Engine/Loaders/Assembly/AssemblyLoader.cs
@@ -1,23 +1,30 @@
 ﻿using ORBIT9000.Core.Abstractions.Plugin;
 using Serilog;
+
 using SystemAssembly = System.Reflection.Assembly;
 
 namespace ORBIT9000.Engine.Loaders.Assembly
 {
-    internal class AssemblyLoader
+    internal sealed class AssemblyLoader
     {
-        private static readonly Serilog.ILogger _logger = Log.Logger.ForContext<AssemblyLoader>();
+        private static readonly Serilog.ILogger _logger
+            = Log.Logger.ForContext<AssemblyLoader>();
 
-        public static (SystemAssembly Assembly, bool ContainsPlugins, Type[] plugins) TryLoadAssembly(string path, List<Exception> exceptions)
+        private AssemblyLoader() { }
+
+        public static AssemblyLoadResult TryLoadAssembly(FileInfo info)
         {
-            var info = new FileInfo(path);
-            SystemAssembly? assembly = null;
             bool containsPlugins = false;
+
+            SystemAssembly? assembly = null;            
+            List<Exception> exceptions = new List<Exception>();
+
             Type[] pluginTypes = Array.Empty<Type>();
 
             try
             {
-                byte[] assemblyBytes = File.ReadAllBytes(path);
+                byte[] assemblyBytes = File.ReadAllBytes(info.FullName);
+
                 assembly = SystemAssembly.Load(assemblyBytes);
                 pluginTypes = assembly.GetTypes()
                     .Where(type => type.IsClass && typeof(IOrbitPlugin).IsAssignableFrom(type))
@@ -32,11 +39,11 @@ namespace ORBIT9000.Engine.Loaders.Assembly
             }
             catch (Exception ex)
             {
-                _logger?.Warning(ex, "Failed to load assembly from {Path}", path);
+                _logger?.Warning(ex, "Failed to load assembly from {Path}", info.FullName);
                 exceptions.Add(ex);
             }
 
-            return (assembly, containsPlugins, pluginTypes);
+            return new(assembly, containsPlugins, pluginTypes, exceptions);
         }
     }
 }
---
+++ src/App/Engine/Loaders/Plugin/Validation/PluginFileValidator.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ORBIT9000.Engine.Loaders.Plugin.Validation
+{
+    /// <summary>
+    /// Helper class to validate plugin files.
+    /// </summary>
+    internal class PluginFileValidator
+    {
+        private readonly ILogger? _logger;
+        private readonly FileInfo _info;
+
+        public PluginFileValidator(FileInfo info, ILogger? logger)
+        {
+            _info = info ?? throw new ArgumentNullException(nameof(info));
+            _logger = logger;
+        }
+
+        public bool FileExists => _info.Exists;
+        public bool IsDll => string.Equals(_info.Extension, ".dll", StringComparison.OrdinalIgnoreCase);
+        public bool IsValid => FileExists && IsDll;
+        public List<Exception> Exceptions = new List<Exception>();  
+
+        /// <summary>
+        /// Validates the plugin file Path and type.
+        /// </summary>
+        /// <returns>A list of exceptions encountered during validation.</returns>
+        public PluginFileValidator Validate()
+        {
+            if (_info == null)
+            {
+                _logger?.LogWarning("FileInfo object is null.");
+                Exceptions.Add(new ArgumentNullException(nameof(_info), "FileInfo object is null."));                
+            }
+
+            if (!FileExists)
+            {
+                _logger?.LogWarning("File does not exist: {FilePath}", _info.FullName);
+                Exceptions.Add(new FileNotFoundException($"File does not exist: {_info.FullName}"));
+            }
+
+            if (!IsDll)
+            {
+                _logger?.LogWarning("File is not a DLL: {FilePath}", _info.FullName);
+                Exceptions.Add(new ArgumentException($"File is not a DLL: {_info.FullName}"));
+            }
+
+            return this;
+        }
+    }
+}
