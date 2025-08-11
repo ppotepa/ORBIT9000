4ebbdb5237b9b5b658b4324773873fe92306f2cf
PPOTEPA-DEV\pawel <potepapawel@gmail.com>
2025-08-11 23:12:52 +0200

    no message

--- src/App/GUI/EngineTerminal/Program.cs
+++ src/App/GUI/EngineTerminal/Program.cs
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿using EngineTerminal.Contracts;
 using EngineTerminal.Managers;
 using Microsoft.Extensions.DependencyInjection;
@@ -70,112 +71,94 @@ using System.Buffers;
 using System.IO.Pipes;
 using Terminal.Gui;
 >>>>>>> 4502f33 (Add GUI BoilerPlate)
+=======
+﻿using Terminal.Gui;
+>>>>>>> f9f63ea (Add Simple Html View Parsing to Terminal PoC)
 
 namespace Orbit9000.EngineTerminal
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Application.Init();
+         
+            var colorScheme = new ColorScheme
+            {
+                Normal = Application.Driver.MakeAttribute(Color.White, Color.Blue),
+                Focus = Application.Driver.MakeAttribute(Color.Gray, Color.DarkGray),
+                HotNormal = Application.Driver.MakeAttribute(Color.BrightBlue, Color.Blue),
+                HotFocus = Application.Driver.MakeAttribute(Color.BrightYellow, Color.DarkGray)
+            };
+
+            Application.Current.ColorScheme = colorScheme;
 
             var top = Application.Top;
 
-            var menu = new MenuBar(new MenuBarItem[]
-            {
-                    new MenuBarItem("Engine", new MenuItem[]
-                    {
-                        new MenuItem("Start", "", () => {}),
-                        new MenuItem("Stop", "", () => {})
-                    }),
+            var html = System.IO.File.ReadAllText("./views/index.html");
 
-                    new MenuBarItem("Plugins", new MenuItem[]
-                    {
-                        new MenuItem("Load", "", () => {}),
-                        new MenuItem("Unload", "", () => {}),
-                        new MenuItem("Activity", "", () => {})
-                    }),
-                    new MenuBarItem("Diagnostics", new MenuItem[]
-                    {
-                        new MenuItem("UpTime", "", () => {})
-                    })
-            });
+            var translator = new Translator();
+            var mainView = translator.Translate(html);
 
-            top.Add(menu);
-
-            var textView = new TextView
-            {
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
-                ReadOnly = true
-            };
-
-            var mainWindow = new MainWindow();
-            top.Add(mainWindow);
-
-            var window = new Window("Engine Terminal")
-            {
-                Width = Dim.Fill(),
-                Height = Dim.Fill()
-            };
-
-            window.Add(textView);
-            top.Add(window);
-
-            var backgroundThread = new Thread(() =>
-            {
-                var client = new NamedPipeClientStream(".", "OrbitEngine", PipeDirection.In);
-
-                try
-                {
-                    client.Connect();
-                    Application.MainLoop.Invoke(() => textView.Text += "Connected to engine!\n");
-
-                    var buffer = new byte[4096];
-
-                    while (true)
-                    {
-                        try
-                        {
-                            int bytesRead = client.Read(buffer, 0, buffer.Length);
-                            if (bytesRead == 0)
-                            {
-                                Application.MainLoop.Invoke(() => textView.Text += "Server closed connection.\n");
-                                break;
-                            }
-
-                            List<PluginInfo> @object = MessagePackSerializer.Deserialize<List<PluginInfo>>(new ReadOnlySequence<byte>(buffer, 0, bytesRead));
-
-                            Application.MainLoop.Invoke(() =>
-                            {
-                                mainWindow.AddText($"Received Engine state: {@object.Count}\n");
-
-                                foreach (var pluginInfo in @object)
-                                {
-                                    textView.Text += $"Plugin: {pluginInfo.PluginType}, Activated: {pluginInfo.Activated}\n";
-                                }
-                            });
-                        }
-                        catch (IOException ex)
-                        {
-                            Application.MainLoop.Invoke(() => textView.Text += $"Pipe broken: {ex.Message}\n");
-                            break;
-                        }
-
-                        Thread.Sleep(1000);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Application.MainLoop.Invoke(() => textView.Text += $"Error connecting to pipe: {ex.Message}\n");
-                }
-            });
-
-            backgroundThread.IsBackground = true;
-            backgroundThread.Start();
+            top.Add(mainView);
 
             Application.Run();
         }
+
+        #region PipeRegion
+        //var backgroundThread = new Thread(() =>
+        //    {
+        //        var client = new NamedPipeClientStream(".", "OrbitEngine", PipeDirection.In);
+
+        //        try
+        //        {
+        //            client.Connect();
+        //            Application.MainLoop.Invoke(() => textView.Text += "Connected to engine!\n");
+
+        //            var buffer = new byte[4096];
+
+        //            while (true)
+        //            {
+        //                try
+        //                {
+        //                    int bytesRead = client.Read(buffer, 0, buffer.Length);
+        //                    if (bytesRead == 0)
+        //                    {
+        //                        Application.MainLoop.Invoke(() => textView.Text += "Server closed connection.\n");
+        //                        break;
+        //                    }
+
+        //                    List<PluginInfo> @object = MessagePackSerializer.Deserialize<List<PluginInfo>>(new ReadOnlySequence<byte>(buffer, 0, bytesRead));
+
+        //                    Application.MainLoop.Invoke(() =>
+        //                    {
+        //                        textView.Text += ($"Received Engine state: {@object.Count}\n");
+
+        //                        foreach (var pluginInfo in @object)
+        //                        {
+        //                            textView.Text += $"Plugin: {pluginInfo.PluginType}, Activated: {pluginInfo.Activated}\n";
+        //                        }
+        //                    });
+        //                }
+        //                catch (IOException ex)
+        //                {
+        //                    Application.MainLoop.Invoke(() => textView.Text += $"Pipe broken: {ex.Message}\n");
+        //                    break;
+        //                }
+
+        //                Thread.Sleep(1000);
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Application.MainLoop.Invoke(() => textView.Text += $"Error connecting to pipe: {ex.Message}\n");
+        //        }
+        //    }); 
+        #endregion
     }
+<<<<<<< HEAD
 }
 >>>>>>> 590e002 (Add Temporary NamedPipe and Receiving Console App)
+=======
+}
+>>>>>>> f9f63ea (Add Simple Html View Parsing to Terminal PoC)
