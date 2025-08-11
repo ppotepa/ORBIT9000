40a4406ff48690980bd79d01ff4d72a2e8caa1b7
PPOTEPA-DEV\pawel <potepapawel@gmail.com>
2025-08-11 23:20:42 +0200

    no message

--- src/Plugins/AutomatedSeleniumRunner/AutomatedSeleniumRunner.cs
+++ src/Plugins/AutomatedSeleniumRunner/AutomatedSeleniumRunner.cs
@@ -4,12 +4,17 @@ using ORBIT9000.Core.Attributes.Engine;
 
 namespace LinkedIn.JobApplier
 {
+<<<<<<< HEAD:src/Plugins/LinkedIn.JobApplier/LinkedInJobApplier.cs
     [SchedulableService("run every 10 seconds")]
     public class LinkedInJobApplier : IOrbitPlugin
+=======
+    [SchedulableService("run every 5 seconds")]
+    public class AutomatedSeleniumRunner : IOrbitPlugin
+>>>>>>> 93105f8 (Adjust Naming):src/Plugins/AutomatedSeleniumRunner/AutomatedSeleniumRunner.cs
     {
-        private readonly ILogger<LinkedInJobApplier> logger;
+        private readonly ILogger<AutomatedSeleniumRunner> logger;
 
-        public LinkedInJobApplier(ILogger<LinkedInJobApplier> logger)
+        public AutomatedSeleniumRunner(ILogger<AutomatedSeleniumRunner> logger)
         {
             this.logger = logger;
         }
